96a616ed1802ba9a1d5a1b95035d929d10c871ca
Lily <lilyhuyixin@g.ucla.edu>
2023-03-12 22:33:40 -0700

    user login
    set up routes and controllers
    check whether email and password exist
    check correctness
    send web token back to client

--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema(
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -49,6 +50,10 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function ( candidatePassword, userPassword ){
+  return await bcrypt.compare(candidatePassword, userPassword)
+};
 
 //create a model out of the schema
 const User = mongoose.model('User', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 router
   .route('/')
